e1f4e65379c0abde8f07e2f0fdba724a72d1d805
Henrique Prado <heprado97@gmail.com>
2026-01-13 23:36:47 -0300

    arrumando disko

--- machines/dev-machine/disko.nix
+++ machines/dev-machine/disko.nix
@@ -1,48 +1,55 @@
 {
-disko.devices = {
-    device = "/dev/sda";  # ← ajuste se for NVMe (/dev/nvme0n1)
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          size = "512M";
-          type = "EF00";  # EFI System Partition
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
+  disko.devices = {
+    disk = {
+      main = {  # nome arbitrário: "main", "my-disk", etc.
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";  # EFI System Partition
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            lvm = {
+              size = "100%";  # ocupa o resto do disco
+              content = {
+                type = "lvm_pv";
+                vg = "vg0";
+              };
+            };
           };
         };
-        lvm = {
-          size = "100%";  # ocupa o resto do disco
-          content = {
-            type = "lvm_pv";
-            vg = "vg0";
+      };
+    };
+
+    lvm_vg = {
+      vg0 = {
+        type = "lvm_vg";
+        lvs = {
+          lv-swap = {
+            size = "2G";
+            content = {
+              type = "swap";
+              randomEncryption = false;
+            };
+          };
+          lv-root = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
           };
         };
       };
     };
   };
-
-  disko.devices.lvm_vg.vg0 = {
-    type = "lvm_vg";
-    lvs = {
-      lv-swap = {
-        size = "2G";
-        content = {
-          type = "swap";
-          randomEncryption = false;  # ou true, se quiser criptografia
-        };
-      };
-      lv-root = {
-        size = "100%FREE"; #Ocupa tudo.
-        content = {
-          type = "filesystem";
-          format = "ext4";
-          mountpoint = "/";
-        };
-      };
-    };
-};
 }

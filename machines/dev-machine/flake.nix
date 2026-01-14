66c8ae99b5cfb4c74dddd5782ea81cab09a46546
Henrique Prado <heprado97@gmail.com>
2026-01-13 23:14:04 -0300

    flake

--- machines/dev-machine/flake.nix
+++ machines/dev-machine/flake.nix
@@ -1,9 +1,14 @@
 {
-  imports = [ "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix" ];
-  
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs.disko.url = "github:nix-community/disko/latest";
+  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
-
-  disko.devices = 
+  outputs = { self, disko, nixpkgs }: {
+    nixosConfigurations.mymachine = nixpkgs.legacyPackages.x86_64-linux.nixos [
+      ./configuration.nix
+      disko.nixosModules.disko
+      {
+              disko.devices = 
   {
     disk = {
       main = { 
@@ -57,8 +62,9 @@
             };
           };
   };
-
-
+        
+      }
+    ];
+  };
 }
-
         

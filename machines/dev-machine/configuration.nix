ef9d62fc38301f75fe5083e043d9ae1cc709f3d1
Henrique Prado de Sousa <lipnose1@gmail.com>
2026-01-15 17:13:58 -0300

    teste

--- machines/dev-machine/configuration.nix
+++ machines/dev-machine/configuration.nix
@@ -1,6 +1,13 @@
-{ config, pkgs, ... }:
+{ config, pkgs, disko, ... }:
 {
+  
+  imports = [
+    ./hardware-configuration.nix 
+    disko.nixosModules.disko
+    ./disko.nix
+  ];
 
+  disko.enable = true;
 
   zramSwap.enable = true;
   zramSwap.memoryPercent = 100;

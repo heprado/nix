8e6de32a9c5d8241fd11448812ebf7a46d692009
Henrique Prado de Sousa <lipnose1@gmail.com>
2026-01-15 23:40:21 -0300

    teste

--- install.sh
+++ install.sh
@@ -1,44 +1,52 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-set -euo pipefail
+# set -euo pipefail
 
-MACHINE="dev-machine";
-
-
-
-export NIX_CONFIG="experimental-features = nix-command flakes"
-
-if [[ ! -d /sys/firmware/efi ]]; then
-
-
-  echo "Erro: Sistema não está em modo UEFI. Hyprland + LVM requer UEFI."
-
-  exit 1
-
-fi
-
-echo "Montando tmpfs em /tmp para builds temporários..."
-
-mount -t tmpfs -o size=8G tmpfs /tmp  
-
-echo "Particionando realmente"
-
-nix shell nixpkgs#disko -c disko --mode disko "./machines/$MACHINE/disko.nix"
-
-nixos-generate-config --no-filesystems --root /mnt 
-
-cp ./machines/$MACHINE/* /mnt/etc/nixos/
-
-cp ./flake.nix /mnt/etc/nixos/
-
-echo "Aplicando configs"
-
-echo "Instalando"
-
-
-nixos-install --flake "/mnt/etc/nixos#dev-machine" --no-root-passwd
+# MACHINE="dev-machine";
 
 
 
+# export NIX_CONFIG="experimental-features = nix-command flakes"
+
+# if [[ ! -d /sys/firmware/efi ]]; then
+
+
+#   echo "Erro: Sistema não está em modo UEFI. Hyprland + LVM requer UEFI."
+
+#   exit 1
+
+# fi
+
+# echo "Montando tmpfs em /tmp para builds temporários..."
+
+# mount -t tmpfs -o size=8G tmpfs /tmp  
+
+# echo "Particionando realmente"
+
+# nix shell nixpkgs#disko -c disko --mode disko "./machines/$MACHINE/disko.nix"
+
+# nixos-generate-config --no-filesystems --root /mnt 
+
+# cp ./machines/$MACHINE/* /mnt/etc/nixos/
+
+# cp ./flake.nix /mnt/etc/nixos/
+
+# echo "Aplicando configs"
+
+# echo "Instalando"
+
+
+# nixos-install --flake "/mnt/etc/nixos#dev-machine" --no-root-passwd
+
+
+FLAKE="github:heprado/nix/flake/dir=machines/dev-machine#dev-machine"
+DISK_DEVICE=/dev/sda
+
+sudo nix \
+    --extra-experimental-features 'flakes nix-command' \
+    run github:nix-community/disko#disko-install -- \
+    --flake "$FLAKE" \
+    --write-efi-boot-entries \
+    --disk main "$DISK_DEVICE"
 
 
---
+++ machines/dev-machine/flake.nix
@@ -0,0 +1,91 @@
+{
+  description = "My NixOS system with disko + LVM + /nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, disko, ... }:
+    let 
+      system = "x86_64_linux";
+      hostname = "dev-machine";
+    in
+    {
+      nixosConfigurations.dev-machine = nixpkgs.lib.nixosSystem {
+        inherit system;
+        system = "x86_64-linux";
+        modules = [
+          ./disko.nix
+          disko.nixosModules.disko
+          ({ pkgs, ... }:{
+            zramSwap.enable = true;
+            zramSwap.memoryPercent = 100;
+
+            # Bootloader UEFI
+            boot.initrd.systemd.enable = true;
+            boot.loader.systemd-boot.enable = true;
+            boot.loader.efi.canTouchEfiVariables = true;
+
+            # LVM
+            services.lvm.enable = true;
+            boot.supportedFilesystems = [ "ext4" ];
+
+            # Desativar X11 (Hyprland é Wayland-only)
+            services.xserver.enable = false;
+
+            # Hyprland
+            programs.hyprland.enable = true;
+
+            # Drivers Intel (Skylake)
+            hardware.graphics.enable = true;
+            hardware.graphics.enable32Bit = true;
+
+            # Firmware adicional
+            hardware.enableRedistributableFirmware = true;
+
+            # Rede
+            networking.networkmanager.enable = true;
+            networking.hostName = "dev-machine";
+
+            # Localização
+            time.timeZone = "America/Sao_Paulo";
+            i18n.defaultLocale = "pt_BR.UTF-8";
+            console.keyMap = "br-abnt2";
+
+            # Usuário
+            # rofi waybar swaybg wl-clipboard grim slurp pavucontrol
+            users.users.heprado = {
+              isNormalUser = true;
+              description = "User";
+              extraGroups = [ "networkmanager" "wheel" ];
+              packages = with pkgs; [
+                firefox alacritty  
+              ];
+              # initialPassword = "changeme";  # descomente se quiser senha fixa
+            };
+
+            # Pacotes globais
+            environment.systemPackages = with pkgs; [
+              vim git curl wget htop xdg-utils lvm2
+            ];
+
+            # Segurança e manutenção
+            nix.gc.automatic = true;
+
+            nix.gc.options = "--delete-older-than 7d";
+
+            system.stateVersion = "24.11";
+          })
+        ];
+      };
+
+      # Optional: provide an installer script
+      # scripts.installDevMachine = {
+      #   description = "Install dev-machine";
+      #   type = "app";
+      #   program = "${self}/machines/dev-machine/install.sh";
+      # };
+    };
+}

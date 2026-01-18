2c16e81d6fb6e71f4a6b4c5d2674930f4e84cc4d
Henrique Prado de Sousa <lipnose1@gmail.com>
2026-01-18 17:44:33 -0300

    Cachix.

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,8 @@
       options = "--delete-older-than 7d";
     };
     settings = {
+      substitutes = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="]
       experimental-features = ["nix-command" "flakes"];
     };
     extraOptions = ''

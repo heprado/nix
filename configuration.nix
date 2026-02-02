b543f72c802bb2a53e9baa798f17375d380b9752
Henrique Prado de Sousa <lipnose1@gmail.com>
2026-02-02 09:42:16 -0300

    teste

--- configuration.nix
+++ configuration.nix
@@ -42,8 +42,8 @@ imports =
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   programs.home-manager.enable = true;

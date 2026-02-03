1f59cd0d1c1b383af91082c35c141debafb5b2b3
Henrique Prado de Sousa <lipnose1@gmail.com>
2026-02-03 11:44:39 -0300

    font

--- configuration.nix
+++ configuration.nix
@@ -73,10 +73,18 @@ imports =
   environment.systemPackages = with pkgs; [
     vim git curl wget lvm2 xfce.thunar waybar rofi foot gtk4 
   ];
-  
+
   fonts.packages = with pkgs; [ 
     nerd-fonts.blex-mono 
   ];
+
+  fontconfig = {
+    defaultFonts = {
+      serif = [  "BlexMono" ];
+      sansSerif = [ "BlexMono" ];
+      monospace = [ "BlexMono" ];
+    };
+  };
 
   environment.variables = {
     XDG_CURRENT_DESKTOP="Hyprland";

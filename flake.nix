d3fb6636af7c85a6b03a4c698123c494e34f7494
Henrique Prado de Sousa <lipnose1@gmail.com>
2026-02-19 19:14:39 -0300

    teste

--- flake.nix
+++ flake.nix
@@ -3,14 +3,13 @@
 
   inputs = {
     
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
